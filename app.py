ef210df85a40e6bf547e7181fdc266c51e52821d
Luke Merrett <luke.merrett@unai.com>
2021-08-20 12:35:05 +0100

    Dummy app file to generate migrations

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:6456/postgres"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    name = db.Column(db.String(128), primary_key=True)
+    date = db.Column(db.DateTime())
+    department = db.Column(db.String(128))
